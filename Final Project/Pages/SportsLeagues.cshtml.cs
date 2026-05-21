f2883aebfd7c56f668f9fde6beaff4abe1f64691
Daniel1833434 <76805752+Daniel1833434@users.noreply.github.com>
2026-05-21 23:01:52 +0300

    Removed BasketballApi

--- Final Project/Pages/SportsLeagues.cshtml.cs
+++ Final Project/Pages/SportsLeagues.cshtml.cs
@@ -9,7 +9,6 @@ namespace Pesach_Project.Pages
         private readonly IConfiguration _configuration;
 
         public List<FootballStanding> FootballTable { get; set; } = new List<FootballStanding>();
-        public List<BasketballStanding> BasketballTable { get; set; } = new List<BasketballStanding>();
 
         public SportsLeaguesModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
@@ -20,7 +19,6 @@ namespace Pesach_Project.Pages
         public async Task OnGetAsync()
         {
             await LoadFootballTable();
-            await LoadBasketballTable();
         }
 
         private async Task LoadFootballTable()
@@ -60,80 +58,6 @@ namespace Pesach_Project.Pages
             }
         }
 
-        private async Task LoadBasketballTable()
-        {
-            string apiKey = _configuration["ApiSports:Key"];
-
-            HttpClient client = _httpClientFactory.CreateClient();
-
-            client.DefaultRequestHeaders.Clear();
-            client.DefaultRequestHeaders.TryAddWithoutValidation("x-apisports-key", apiKey);
-
-            string url = "https://v1.basketball.api-sports.io/standings?league=12&season=2024-2025X";
-
-            string json = await client.GetStringAsync(url);
-
-            JsonDocument document = JsonDocument.Parse(json);
-
-            JsonElement responseArray = document
-                .RootElement
-                .GetProperty("response");
-
-            List<BasketballStanding> tempTable = new List<BasketballStanding>();
-            HashSet<int> addedTeams = new HashSet<int>();
-
-            foreach (JsonElement group in responseArray.EnumerateArray())
-            {
-                if (group.ValueKind == JsonValueKind.Array)
-                {
-                    foreach (JsonElement row in group.EnumerateArray())
-                    {
-                        AddBasketballRow(row, tempTable, addedTeams);
-                    }
-                }
-                else if (group.ValueKind == JsonValueKind.Object)
-                {
-                    AddBasketballRow(group, tempTable, addedTeams);
-                }
-            }
-
-            BasketballTable = tempTable
-                .OrderByDescending(team => team.Wins)
-                .ThenBy(team => team.Losses)
-                .ThenByDescending(team => team.PointsFor - team.PointsAgainst)
-                .ToList();
-
-            for (int i = 0; i < BasketballTable.Count; i++)
-            {
-                BasketballTable[i].Rank = i + 1;
-            }
-        }
-
-        private void AddBasketballRow(JsonElement row, List<BasketballStanding> tempTable, HashSet<int> addedTeams)
-        {
-            int teamId = row.GetProperty("team").GetProperty("id").GetInt32();
-
-            if (addedTeams.Contains(teamId))
-            {
-                return;
-            }
-
-            addedTeams.Add(teamId);
-
-            int pointsFor = row.GetProperty("points").GetProperty("for").GetInt32();
-            int pointsAgainst = row.GetProperty("points").GetProperty("against").GetInt32();
-
-            tempTable.Add(new BasketballStanding
-            {
-                TeamName = row.GetProperty("team").GetProperty("name").GetString(),
-                Played = row.GetProperty("games").GetProperty("played").GetInt32(),
-                Wins = row.GetProperty("games").GetProperty("win").GetProperty("total").GetInt32(),
-                Losses = row.GetProperty("games").GetProperty("lose").GetProperty("total").GetInt32(),
-                PointsFor = pointsFor,
-                PointsAgainst = pointsAgainst
-            });
-        }
-
         public class FootballStanding
         {
             public int Rank { get; set; }
@@ -148,15 +72,6 @@ namespace Pesach_Project.Pages
             public int Points { get; set; }
         }
 
-        public class BasketballStanding
-        {
-            public int Rank { get; set; }
-            public string TeamName { get; set; }
-            public int Played { get; set; }
-            public int Wins { get; set; }
-            public int Losses { get; set; }
-            public int PointsFor { get; set; }
-            public int PointsAgainst { get; set; }
-        }
+      
     }
 }
